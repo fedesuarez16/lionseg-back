167dae0037dc7776f9cd9c1592bd43aac162f51e
fedesuarez16 <fedesuarez16@gmail.com>
2025-03-08 02:18:20 -0300

    idk

--- index.js
+++ index.js
@@ -20,9 +20,7 @@
 
   // Configuración de CORS
   app.use(cors({
-    origin: ['https://lionseg-erp.vercel.app'
-      
-    ]
+    origin: ['https://lionseg-erp.vercel.app', 'http://localhost:3000'] // Añade todos los orígenes permitidos aquí
   }));
   
 
